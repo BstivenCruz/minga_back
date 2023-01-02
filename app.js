9d66283dd3b0fa8ddb805a655ad6f389f58ce97e
ignacioborraz <ignacioborraz@hotmail.com>
2023-01-02 18:48:00 -0300

    fix

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import  { __dirname }  from './utils.js'
 import indexRouter from './routes/index.js';
-import usersRouter from './routes/users.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js'
+import { errorHandler } from './middlewares/errorHandler.js'
+
 
 let app = express();
 
@@ -15,29 +17,14 @@ let app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use("/api", indexRouter)
+app.use(notFoundHandler)
+app.use(errorHandler)
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-export default app;
+export default app
