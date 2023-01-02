d4fd1a7b9e2e27a8856b0eaa606a26c84af3577a
BstivenCruz <bseguracruz@yahoo.com>
2023-01-02 12:09:13 -0500

    config-app

---
+++ config/database.js
@@ -0,0 +1,6 @@
+import mongoose from 'mongoose'
+
+mongoose.set('strictQuery', false)
+mongoose.connect(process.env.MONGO_URI) //MONGO_URI es la uri de conexión
+    .then(() => console.log('database connected'))
+    .catch(err => console.log(err))
