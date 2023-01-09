a0387e6cd8b8fb4549f0cc09e17458c3dca6589b
martinrioss <martinrios.rrr@gail.com>
2023-01-09 15:16:18 -0300

    back listo

--- schemas/comics.schema.js
+++ schemas/comics.schema.js
@@ -2,20 +2,26 @@ import Joi from "joi-oid";
 
 const comicSchema=Joi.object({
     author_id:Joi.objectId().required().messages({
-        "any.required":"the field is required"
+        "any.required":"The field is required",
+        "string.empty":"The field Author is empty",
     }),
     company_id:Joi.objectId(),
     title:Joi.string().required().messages({
-        "any.required":"the field is required"
+        "any.required":"The field is required",
+        "string.empty":"The field Title is empty",
     }),
     photo:Joi.string().required().messages({
-        "any.required":"the field is required"
+        "any.required":"The field is required",
+        "string.empty":"The field Photo is empty",
+
     }),
     description:Joi.string().required().messages({
-        "any.required":"the field is required"
+        "any.required":"The field is required",
+        "string.empty":"The field Description is empty",
     }),
     category:Joi.objectId().required().messages({
-        "any.required":"the field is required"
+        "any.required":"The field is required",
+        "string.empty":"The field Category is empty",
     })
 })
 
