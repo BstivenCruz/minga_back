dcc629211f19abff439e6a569b3195c57059cdad
BstivenCruz <90658527+BstivenCruz@users.noreply.github.com>
2023-01-17 15:36:07 -0500

    Comics-3 (#36)
    * controllerget
    
    * controladores y prueba postman listo
    
    * casi la embarro
    
    * populate, lo ultimo
    
    * comics-3 (#35)
    
    * data
    
    * data
    
    * data
    
    * correcciones
    
    Co-authored-by: ignacioborraz <ignacioborraz@hotmail.com>
    
    Co-authored-by: ignacioborraz <ignacioborraz@hotmail.com>

--- controllers/comics.controller.js
+++ controllers/comics.controller.js
@@ -5,7 +5,7 @@ const controller={
     create:async(req,res,next)=>{
         try{
             const {author_id,company_id, title,photo,description,category}=req.body
-            let comic= await Comic.create({author_id,company_id, title,photo,description,category})
+            await Comic.create({author_id,company_id, title,photo,description,category})
             res.status(201).json({
                 success: true,
                 response: 'done',
@@ -13,6 +13,5 @@ const controller={
         }catch(err){
            next(err)
         }
-    },
-}
+}}
 export default controller
--- routes/chapter.route.js
+++ routes/chapter.route.js
@@ -1,12 +1,15 @@
-import controller from "../controllers/chapter.controller.js";
 import  express  from "express";
+let router = express.Router();
+import controller from "../controllers/chapter.controller.js";
 import schema from "../schemas/chapter.schema.js";
 import validator from "../middlewares/validator.js";
 import orderExists from '../middlewares/orderExist.js';
+import controllerDetails from '../controllers/chapter.details.controller.js'
+const { get_comics_chapters} = controllerDetails
 
-let router = express.Router();
-const {create} = controller
+const {create } = controller
 
 router.post('/',validator(schema), orderExists, create)
+router.get('/',get_comics_chapters)
 
 export default router; 
