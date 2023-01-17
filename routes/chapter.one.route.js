3f88c84f0444f1a7be72908d2e5749585b7e5850
Agustin <acaceres107@gmail.com>
2023-01-17 17:16:39 -0300

    borrado de archivos sobrantes

--- routes/chapter.one.route.js
+++
@@ -1,8 +0,0 @@
-import  express  from "express";
-import controller from "../controllers/chapter.controller.js";
-
-let router = express.Router();
-const {read} = controller
-router.post('/', read)
-
-export default router; 
