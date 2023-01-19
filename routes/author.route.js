c0df3aa400cf4d758c462cf25689a47d8c8d2bd8
Agustin <acaceres107@gmail.com>
2023-01-19 17:51:11 -0300

    passport, casi terminado

--- routes/author.route.js
+++ routes/author.route.js
@@ -5,12 +5,15 @@ let router = express.Router()
 import authorSchema from "../schemas/author.schema.js"
 import validator from "../middlewares/validator.js"
 import authorActive from "../middlewares/authorActive.js"
+import passport from "passport"
+import isAuthor from "../middlewares/isAuthor.js"
+
 
 //controller
 import controller from "../controllers/author.controller.js"
 const { create } = controller
 
 //endpoints
-router.post('/', validator(authorSchema),authorActive, create)
+router.post('/',passport.authenticate("jwt",{session:false}), isAuthor,authorActive, validator(authorSchema),authorActive, create)
   
 export default router
--- routes/chapter.route.js
+++ routes/chapter.route.js
@@ -1,22 +1,24 @@
 import  express  from "express"
 let router = express.Router()
-
+import passport from "passport"
 //schemas & middlewares
 import schema from "../schemas/chapter.schema.js"
 import validator from "../middlewares/validator.js"
 import orderExists from '../middlewares/orderExist.js'
-
+import isAdmin from "../middlewares/isAdmin.js"
+import authorActive from "../middlewares/authorActive.js"
 //controller
 import chapter_controller from "../controllers/chapter.controller.js"
 const { create,get_pages } = chapter_controller
 import all_controller from '../controllers/chapter.all.controller.js'
 const { get_comics_chapters } = all_controller
 import details_controller from '../controllers/chapter.details.controller.js'
+import isAuthor from "../middlewares/isAuthor.js"
 const { get_pages_from_chapter } = details_controller
 
-router.post('/',validator(schema), orderExists, create)
-router.get('/', get_comics_chapters)
-router.get('/order', get_pages_from_chapter)
-router.get('/pages/:_id', get_pages)
+router.post('/',passport.authenticate("jwt",{session:false}),isAuthor,isAdmin,authorActive,validator(schema), orderExists, create)
+router.get('/',passport.authenticate("jwt",{session:false}), get_comics_chapters)
+router.get('/order',passport.authenticate("jwt",{session:false}), get_pages_from_chapter)
+router.get('/pages/:_id',passport.authenticate("jwt",{session:false}), get_pages)
 
 export default router
