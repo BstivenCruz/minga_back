ef44c012e226d1b62432c49422da0f311d2c5b45
BstivenCruz <bseguracruz@yahoo.com>
2023-01-19 13:37:58 -0500

    controladores up and des

--- routes/chapter.route.js
+++ routes/chapter.route.js
@@ -1,22 +1,28 @@
-import  express  from "express"
-let router = express.Router()
+import express from "express";
+let router = express.Router();
 
 //schemas & middlewares
-import schema from "../schemas/chapter.schema.js"
-import validator from "../middlewares/validator.js"
-import orderExists from '../middlewares/orderExist.js'
+import schema from "../schemas/chapter.schema.js";
+import validator from "../middlewares/validator.js";
+import orderExists from "../middlewares/orderExist.js";
+import passport from "../config/passport.js"
+
 
 //controller
-import chapter_controller from "../controllers/chapter.controller.js"
-const { create,get_pages } = chapter_controller
-import all_controller from '../controllers/chapter.all.controller.js'
-const { get_comics_chapters } = all_controller
-import details_controller from '../controllers/chapter.details.controller.js'
-const { get_pages_from_chapter } = details_controller
+import chapter_controller from "../controllers/chapter.controller.js";
+const { create, get_pages, get_one_chapter, update, destroy } = chapter_controller;
+import all_controller from "../controllers/chapter.all.controller.js";
+const { get_comics_chapters } = all_controller;
+import details_controller from "../controllers/chapter.details.controller.js";
+import { Passport } from "passport";
+const { get_pages_from_chapter } = details_controller;
 
-router.post('/',validator(schema), orderExists, create)
-router.get('/', get_comics_chapters)
-router.get('/order', get_pages_from_chapter)
-router.get('/pages/:_id', get_pages)
+router.post("/", passport.authenticate( "jwt" ,{session : false}) ,validator(schema), orderExists, create);
+router.get("/", passport.authenticate( "jwt" ,{session : false}) ,get_comics_chapters);
+router.get("/:id", passport.authenticate( "jwt" , { sesssion : true}) , get_one_chapter)
+router.get("/order", passport.authenticate( "jwt" ,{session : false}) ,get_pages_from_chapter);
+router.get("/pages/:_id", passport.authenticate( "jwt" ,{session : false}) ,get_pages);
+router.put("/:id",passport.authenticate( "jwt" , {session : false}), update)
+router.delete("/:id",  passport.authenticate( "jwt" ,{session : false}) , destroy)
 
-export default router
+export default router;
